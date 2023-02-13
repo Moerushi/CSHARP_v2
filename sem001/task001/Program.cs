3bd9e0e016561080f1ae115dd2048a563073a0cd
Nikita Matveev <nikitamatveev92@gmail.com>
2023-02-13 23:03:00 +1000

    Sem001/Task001 solved

---
+++ sem001/task001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+  int result = (number / 10) % 10;
+  Console.WriteLine("Вторая цифра в Вашем числе: " + result);
+}
+else
+{
+  Console.WriteLine("Некорректное число");
+}
