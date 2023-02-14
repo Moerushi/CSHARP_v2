0eac32994f1207e9a328c85074b8d538049b954d
Nikita Matveev <nikitamatveev92@gmail.com>
2023-02-14 23:10:21 +1000

    Add structure to readme and solution for first and third task of sem002

---
+++ sem002/task001/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит случайное число трехзначное число
+// и удаляет вторую цифру этого числа.
+// 456 - 46
+// 782 - 72
+// 918 - 98
+
+Console.Clear();
+
+string ExtractNum(int num1)
+{
+int a = num1 / 100;
+int c = num1 % 10;
+
+string ac = "Результат удаления второй цифры числа: " + (a * 10 + c);
+return ac;
+}
+
+int value = new Random().Next(100,1000);
+
+Console.WriteLine("Случайное число: " + value);
+Console.WriteLine(ExtractNum(value));
+
---
+++ sem002/task003/Program.cs
@@ -0,0 +1,30 @@
+﻿// 3 Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+// то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+string EvenNum(int arg1, int arg2)
+{
+if (arg1 % arg2 == 0)
+{
+  string resultEven;
+  return resultEven = "Кратно";
+}
+else
+{
+  string resultOdd;
+  return resultOdd = "Не кратно, остаток = " + (arg1 % arg2);
+}
+}
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(EvenNum(firstNumber, secondNumber));
